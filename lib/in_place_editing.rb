4f4c1092d4e92187151115df6b86fb35452f41fc
Noah Davis <noahd1@yahoo.com>
2010-12-12 01:15:20 -0500

    initial commit extracted from weplay plugin; plugin was not a submodule, but based on rails plugin with modifications

---
+++ lib/in_place_editing.rb
@@ -0,0 +1,29 @@
+module InPlaceEditing
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  # Example:
+  #
+  #   # Controller
+  #   class BlogController < ApplicationController
+  #     in_place_edit_for :post, :title
+  #   end
+  #
+  #   # View
+  #   <%= in_place_editor_field :post, 'title' %>
+  #
+  module ClassMethods
+    def in_place_edit_for(object, attribute, options = {})
+      define_method("set_#{object}_#{attribute}") do
+        @item = object.to_s.camelize.constantize.find(params[:id])
+         if options[:callback].nil?
+           @item.update_attribute(attribute, params[:value])
+           render :text => @item.send(attribute).to_s
+         else
+           method(options[:callback]).call(params[:value])
+         end
+      end
+    end
+  end
+end
